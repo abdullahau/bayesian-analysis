96eb9d6ec172ae2dc4f799d4da15e076f122fc67
abdullahau <abdullah.au@outlook.com>
2025-01-31 16:40:22 +0400

    update

--- init_notebook.py
+++ init_notebook.py
@@ -57,5 +57,5 @@ import warnings  # noqa: E402
 warnings.filterwarnings("ignore")
 
 from matplotlib import style  # noqa: E402
-STYLE = "statistical-rethinking-2023.mplstyle"
+STYLE = "PlottingStyle.mplstyle"
 style.use(STYLE)
